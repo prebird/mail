2b7497dcdfd8bdb2343fe409ffe2dd134624ba64
prebird <78974381+prebird@users.noreply.github.com>
2024-07-12 18:29:12 +0900

    feat: truncate.sql 추가

--- shop/src/main/java/org/prebird/shop/DataInit.java
+++ shop/src/main/java/org/prebird/shop/DataInit.java
@@ -1,11 +1,17 @@
 package org.prebird.shop;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.prebird.shop.product.Product;
 import org.prebird.shop.product.ProductRepository;
 import org.prebird.shop.member.Member;
 import org.prebird.shop.member.MemberRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -13,9 +19,12 @@ import org.springframework.stereotype.Component;
 public class DataInit implements CommandLineRunner {
   private final MemberRepository memberRepository;
   private final ProductRepository productRepository;
+  private final DataSource dataSource;
 
   @Override
   public void run(String... args) throws Exception {
+    truncate();
+
     memberRepository.saveAndFlush(Member.builder()
             .username("tester01")
             .email("iopengom@naver.com")
@@ -26,5 +35,16 @@ public class DataInit implements CommandLineRunner {
             .name("사과")
             .price(1000)
         .build());
+  }
+
+  private void truncate() {
+    Resource resource = new ClassPathResource("truncate.sql");
+    try (Connection connection = dataSource.getConnection()) {
+      ScriptUtils.executeSqlScript(connection, resource);
+      System.out.println("Executed truncate.sql");
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new RuntimeException("Failed to execute truncate.sql", e);
+    }
   }
 }
---
+++ shop/src/main/resources/truncate.sql
@@ -0,0 +1,6 @@
+SET FOREIGN_KEY_CHECKS = 0;
+truncate table member;
+truncate table orders;
+truncate table product;
+truncate table email_message;
+SET FOREIGN_KEY_CHECKS = 1;
