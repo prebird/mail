7199cd79b372c67b910d9d5088ea7b65cd77b4ab
prebird <78974381+prebird@users.noreply.github.com>
2024-07-12 16:29:55 +0900

    feat: mail-worker 프로젝트 추가

---
+++ mail-worker/src/main/java/org/prebird/mailworker/MailWorkerApplication.java
@@ -0,0 +1,13 @@
+package org.prebird.mailworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailWorkerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MailWorkerApplication.class, args);
+  }
+
+}
---
+++ mail-worker/src/test/java/org/prebird/mailworker/MailWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prebird.mailworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailWorkerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
