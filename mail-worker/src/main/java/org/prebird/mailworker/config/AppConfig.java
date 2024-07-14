f35a60fc59944fbb88f8d49ca5c0048518d0e29e
prebird <78974381+prebird@users.noreply.github.com>
2024-07-14 10:01:11 +0900

    feat: ThreadPoolTaskExecutor 추가

--- mail-worker/src/main/java/org/prebird/mailworker/config/AppConfig.java
+++ mail-worker/src/main/java/org/prebird/mailworker/config/AppConfig.java
@@ -1,12 +1,23 @@
 package org.prebird.mailworker.config;
 
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableAsync
 @EnableScheduling
 @Configuration
 public class AppConfig {
 
+  @Bean
+  public Executor taskExecutor() {
+    ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+    taskExecutor.setCorePoolSize(30);
+    taskExecutor.setMaxPoolSize(30);
+    taskExecutor.initialize();
+    return taskExecutor;
+  }
 }
