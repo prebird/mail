e739ca67d3160d7bce5ef1d64d3a89aa6c51fa47
prebird <78974381+prebird@users.noreply.github.com>
2024-07-18 20:06:54 +0900

    feat: 긴급/일반 타입별 JavaMailService 분리

---
+++ mail-worker/src/main/java/org/prebird/mailworker/config/mail/MailSenderConfig.java
@@ -0,0 +1,68 @@
+package org.prebird.mailworker.config.mail;
+
+import java.util.Map;
+import java.util.Properties;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+/**
+ * JavaMailSender 를 빈으로 등록합니다.
+ */
+@Configuration
+public class MailSenderConfig {
+  /**
+   * 긴급 타입 메일 설정으로 설정된 JavaMailSender
+   * @param urgentMailProperties
+   * @return
+   */
+  @Profile("real")
+  @Bean
+  public JavaMailSender urgentJavaMailSender(UrgentMailProperties urgentMailProperties) {
+    return createMailSender(urgentMailProperties);
+  }
+
+  /**
+   * 일반 메일 설정으로 설정된 JavaMailSender
+   * @param normalMailProperties
+   * @return
+   */
+  @Profile("real")
+  @Bean
+  public JavaMailSender normalJavaMailSender(NormalMailProperties normalMailProperties) {
+    return createMailSender(normalMailProperties);
+  }
+
+  private JavaMailSenderImpl createMailSender(MailProperties properties) {
+    JavaMailSenderImpl sender = new JavaMailSenderImpl();
+    this.applyProperties(properties, sender);
+    return sender;
+  }
+
+  private void applyProperties(MailProperties properties, JavaMailSenderImpl sender) {
+    sender.setHost(properties.getHost());
+    if (properties.getPort() != null) {
+      sender.setPort(properties.getPort());
+    }
+
+    sender.setUsername(properties.getUsername());
+    sender.setPassword(properties.getPassword());
+    sender.setProtocol(properties.getProtocol());
+    if (properties.getDefaultEncoding() != null) {
+      sender.setDefaultEncoding(properties.getDefaultEncoding().name());
+    }
+
+    if (!properties.getProperties().isEmpty()) {
+      sender.setJavaMailProperties(this.asProperties(properties.getProperties()));
+    }
+  }
+
+  private Properties asProperties(Map<String, String> source) {
+    Properties properties = new Properties();
+    properties.putAll(source);
+    return properties;
+  }
+}
--- mail-worker/src/main/java/org/prebird/mailworker/service/JavaMailService.java
+++ mail-worker/src/main/java/org/prebird/mailworker/service/JavaMailService.java
@@ -5,17 +5,13 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.prebird.mailworker.domain.EmailMessage;
-import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
 
-@Profile({"real", "local-test"})
 @Slf4j
 @RequiredArgsConstructor
-@Service
 public class JavaMailService implements MailService{
-  private final JavaMailSender javaMailSender;
+  private final JavaMailSender normalJavaMailSender;
 
   /**
    * Java Mail Sender 로 실제로 메일을 보냅니다.
@@ -25,13 +21,13 @@ public class JavaMailService implements MailService{
   public void send(EmailMessage emailMessage) {
     long startTime = System.currentTimeMillis();
 
-    MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+    MimeMessage mimeMessage = normalJavaMailSender.createMimeMessage();
     try {
       MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
       mimeMessageHelper.setTo(emailMessage.getToEmail());
       mimeMessageHelper.setSubject(emailMessage.getSubject());
       mimeMessageHelper.setText(emailMessage.getMessage(), true);
-      javaMailSender.send(mimeMessage);
+      normalJavaMailSender.send(mimeMessage);
 
       // 시간 측정
       long endTime = System.currentTimeMillis();
