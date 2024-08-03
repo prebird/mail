86cee99aafc13c14d831ccaf8d5164ad81ce1955
prebird <78974381+prebird@users.noreply.github.com>
2024-08-03 15:22:55 +0900

    feat(mail-worker): consoleMailService Rate Limit 추가

--- mail-worker/src/main/java/org/prebird/mailworker/mailWorker/MessageQueueMailWorker/MQMailWorker.java
+++ mail-worker/src/main/java/org/prebird/mailworker/mailWorker/MessageQueueMailWorker/MQMailWorker.java
@@ -1,5 +1,6 @@
 package org.prebird.mailworker.mailWorker.MessageQueueMailWorker;
 
+import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.prebird.mailworker.domain.EmailMessage;
@@ -17,39 +18,39 @@ public class MQMailWorker {
   private final MailService mailService;
   public static final String NORMAL_MAIL_QUEUE = "normal.mail.queue";
 
-//  @RabbitListener(queues = {NORMAL_MAIL_QUEUE})
-//  public CompletableFuture<Void> fetchMessageAndSendMail(EmailMessage emailMessage) {
-//    return CompletableFuture.runAsync(() -> {
-//      log.info("--------------------------------");
-//      log.info("fetchMessageAndSendMail called");
-//      log.info("message: {}", emailMessage);
+  @RabbitListener(queues = {NORMAL_MAIL_QUEUE})
+  public CompletableFuture<Void> fetchMessageAndSendMail(EmailMessage emailMessage) {
+    return CompletableFuture.runAsync(() -> {
+      log.info("--------------------------------");
+      log.info("fetchMessageAndSendMail called");
+      log.info("message: {}", emailMessage);
 //      try {
 //        Thread.sleep(2000);
 //      } catch (InterruptedException e) {
 //        throw new AmqpRejectAndDontRequeueException(e);
 //      }
 //      throw new AmqpRejectAndDontRequeueException("에러 발생");
-//      //mailService.send(emailMessage);
-//    });
-//  }
-
-  /**
-   * 동기방식 메세지 소비 Listener
-   * @param emailMessage
-   */
-  @RabbitListener(queues = {NORMAL_MAIL_QUEUE})
-  public void fetchMessageAndSendMailSync(EmailMessage emailMessage) {
-
-    log.info("--------------------------------");
-    log.info("fetchMessageAndSendMail called");
-    log.info("message: {}", emailMessage);
-    try {
-      Thread.sleep(2000);
-    } catch (InterruptedException e) {
-      throw new AmqpRejectAndDontRequeueException(e);
-    }
-    throw new RuntimeException("에러 발생");
-    //mailService.send(emailMessage);
-
+      mailService.send(emailMessage);
+    });
   }
+
+//  /**
+//   * 동기방식 메세지 소비 Listener
+//   * @param emailMessage
+//   */
+//  @RabbitListener(queues = {NORMAL_MAIL_QUEUE})
+//  public void fetchMessageAndSendMailSync(EmailMessage emailMessage) {
+//
+//    log.info("--------------------------------");
+//    log.info("fetchMessageAndSendMail called");
+//    log.info("message: {}", emailMessage);
+//    try {
+//      Thread.sleep(2000);
+//    } catch (InterruptedException e) {
+//      throw new AmqpRejectAndDontRequeueException(e);
+//    }
+//    throw new RuntimeException("에러 발생");
+//    //mailService.send(emailMessage);
+//
+//  }
 }
--- mail-worker/src/main/java/org/prebird/mailworker/service/ConsoleMailService.java
+++ mail-worker/src/main/java/org/prebird/mailworker/service/ConsoleMailService.java
@@ -1,5 +1,6 @@
 package org.prebird.mailworker.service;
 
+import com.google.common.util.concurrent.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.prebird.mailworker.domain.EmailMessage;
 import org.springframework.context.annotation.Profile;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 @Profile("message-queue-console")
 @Slf4j
 public class ConsoleMailService implements MailService {
+  // 초당 5개로 요청 제한
+  private RateLimiter rateLimiter = RateLimiter.create(3);
 
   /**
    * 메일 발송 로직을 콘솔 로그로 대체합니다.
@@ -17,11 +20,19 @@ public class ConsoleMailService implements MailService {
    */
   @Override
   public void send(EmailMessage emailMessage) {
+    limitRequest(); // 요청 제한
+
     try {
-      Thread.sleep(4100);
+      Thread.sleep(4100); // gmail 평균 메세지 처리 시간 만큼 sleep
       log.info(">>> mail sent!");
     } catch (Exception e) {
       log.error("에러 발생");
+    }
+  }
+
+  private void limitRequest() {
+    if (!rateLimiter.tryAcquire()) {
+      throw new IllegalStateException("요청 제한: 나중에 다시 시도하세요");
     }
   }
 }
