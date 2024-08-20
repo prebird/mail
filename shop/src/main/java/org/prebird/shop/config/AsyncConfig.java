2ddcfaee53adcb542fd7e4b4f5409c831bcc3e08
prebird <78974381+prebird@users.noreply.github.com>
2024-08-20 12:04:29 +0900

    feat(shop): 비동기 설정 yml 으로 설정하도록 변경

--- shop/src/main/java/org/prebird/shop/config/AsyncConfig.java
+++ shop/src/main/java/org/prebird/shop/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package org.prebird.shop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -9,12 +10,20 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+  @Value("${mail.async.core-pool-size}")
+  private Integer mailCorePoolSize;
+  @Value("${mail.async.max-pool-size}")
+  private Integer mailMaxPoolSize;
+  @Value("${mail.async.queue-size}")
+  private Integer mailQueueSize;
+
   @Primary
   @Bean
   public ThreadPoolTaskExecutor mailServiceTaskExecutor() {
     ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-    taskExecutor.setCorePoolSize(10);
-    taskExecutor.setMaxPoolSize(10);
+    taskExecutor.setCorePoolSize(mailCorePoolSize);
+    taskExecutor.setMaxPoolSize(mailMaxPoolSize);
+    taskExecutor.setQueueCapacity(mailQueueSize);
     taskExecutor.setPrestartAllCoreThreads(true); // 스레드풀 코어 수 만큼 미리 생성
     taskExecutor.setThreadNamePrefix("mailExecutor-");
     taskExecutor.initialize();
