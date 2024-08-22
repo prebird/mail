d7909c7a7a5ec923d0aa35322a441be532fd64c8
prebird <78974381+prebird@users.noreply.github.com>
2024-08-22 12:40:46 +0900

    refactor(load-test): tps 차트 리팩토링

--- load-tester/src/main/resources/static/load-test.js
+++ load-tester/src/main/resources/static/load-test.js
@@ -1,20 +1,18 @@
 const loadTest = {
-  loadTestSummaryList: []
-  ,tpsChart: undefined
+  tpsChart: undefined
 
   , getLoadTestSummary: (row) => {
     const id = row.getAttribute("data-id");  // 클릭한 행의 id 값 가져오기
     axios.get(`/load-tests/${id}/tps`)
     .then(response => {
       console.log(response.data);
-      // loadTest.loadTestSummaryList = response.data;
-      loadTest.updateChart(response.data);
+      loadTest.updateTpsChart(response.data);
     })
     .catch(error => {
       console.error('Error fetching summary:', error);
     });
   }
-  , updateChart: (tpsData) => {
+  , updateTpsChart: (tpsData) => {
     const labels = tpsData.map(item => item.time);
     const tps = tpsData.map(item => item.tps);
 
@@ -22,40 +20,42 @@ const loadTest = {
     if (loadTest.tpsChart) {
       loadTest.tpsChart.destroy(); // 이전 차트를 제거합니다.
     }
-
-    loadTest.tpsChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'TPS',
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          data: tps,
-          fill: false,
-          pointRadius: 3, // 점 크기 설정
-          pointHoverRadius: 5, // 마우스 오버 시 점 크기 설정
-          borderWidth: 2, // 선 두께 설정
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          x: {
-            title: {
-              display: true,
-              text: 'Elapsed Time (s)'
-            }
-          },
-          y: {
-            title: {
-              display: true,
-              text: 'TPS'
-            },
-            beginAtZero: true
+    loadTest.tpsChart = loadTest.getChart(ctx, labels, tps, 'TPS', 'TPS', 'elapsed time (s)');
+  }
+  , getChart: (ctx, labels, data, dataLabel  ,yLabel, xLabel) => {
+  return new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: dataLabel,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        data: data,
+        fill: false,
+        pointRadius: 3, // 점 크기 설정
+        pointHoverRadius: 5, // 마우스 오버 시 점 크기 설정
+        borderWidth: 2, // 선 두께 설정
+      }]
+    },
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: xLabel
           }
+        },
+        y: {
+          title: {
+            display: true,
+            text: yLabel
+          },
+          beginAtZero: true
         }
       }
-    });
-  }
+    }
+  });
+}
 }
